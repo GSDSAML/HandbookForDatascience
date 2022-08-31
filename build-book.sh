6ddfc71c170cd517cb329aa37a231f114e27ce9a
simonjisu <simonjisu@gmail.com>
2022-08-31 21:18:33 +0900

    update

--- build-book.sh
+++ build-book.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-jupyter-book build book --path-output ./
-ghp-import -n -p -f _build/html
-git add .
-git commit -m 'jupyter-book publish'
-git push origin main
+mkdocs build
+mkdocs gh-deploy --force
